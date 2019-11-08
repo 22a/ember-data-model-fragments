d2a51b3a0dc9e6954e809c79caf91662a2de813b
Peter Meehan <peter.meehan@intercom.io>
2019-11-08 13:24:19 +0000

    Revert "migrate dummy model to native class w/o decorators"
    This reverts commit 3f6d6d5e2bea52216cfbe6ff88203c1a5d4cd962.

--- tests/dummy/app/models/user.js
+++ tests/dummy/app/models/user.js
@@ -1,7 +1,8 @@
 import DS from 'ember-data';
 import MF from 'ember-data-model-fragments';
 
-export default class UserModel extends DS.Model {
-  info = MF.fragment('info');
-  orders = MF.fragmentArray('order');
-}
+export default DS.Model.extend({
+  info: MF.fragment('info'),
+  orders: MF.fragmentArray('order')
+});
+

3f6d6d5e2bea52216cfbe6ff88203c1a5d4cd962
Peter Meehan <peter.meehan@intercom.io>
2019-11-08 02:52:37 +0000

    migrate dummy model to native class w/o decorators

--- tests/dummy/app/models/user.js
+++ tests/dummy/app/models/user.js
@@ -1,8 +1,7 @@
 import DS from 'ember-data';
 import MF from 'ember-data-model-fragments';
 
-export default DS.Model.extend({
-  info: MF.fragment('info'),
-  orders: MF.fragmentArray('order')
-});
-
+export default class UserModel extends DS.Model {
+  info = MF.fragment('info');
+  orders = MF.fragmentArray('order');
+}
